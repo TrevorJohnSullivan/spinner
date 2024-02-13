b7a65f27bd75400d927383b6d0011d0cbb2f095c
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-13 10:45:06 -0330

    1stV

---
+++ spinner2.js
@@ -0,0 +1,22 @@
+const frames = ['|', '/', '-', '\\'];
+let time = 100
+let index = 0;
+for (let i = 0; i < frames.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(`\r${frames[index]}   `);
+    index = (index + 1) % frames.length;
+  }, time);
+  time = time + 200
+}
+
+
+// const frames = ['|', '/', '-', '\\'];
+// let time = 100
+// let index = 0;
+// for (let i = 0; i < frames.length; i++) {
+//   setTimeout(() => {
+//     process.stdout.write(`\r${frames[index]}   `);
+//   }, time);
+//   index++ 
+//   time = time + 200
+// }
